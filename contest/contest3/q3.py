d5667634844ce963ddbb5f87c5b75d000cffc0a8
blen bizuayehu <blenaqua57js@gmail.com>
2026-02-12 22:49:16 +0300

    sum

---
+++ contest/contest3/q3.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+for _ in range(t):
+    n, m, p, q = map(int, input().split())
+
+    if n % p == 0:
+        total = (n // p) * q
+        if total == m:
+            print("YES")
+        else:
+            print("NO")
+    else:
+        print("YES")
---
+++ contest/contest3/q4.py
@@ -0,0 +1,12 @@
+n=int(input())
+
+genome="ACTG"
+
+s=str(input())
+alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+if genome in s:
+    print(0)
+
+# else:
+#     for 
